237d62f61a01bde50639a9e88b48aec7ba937526
Shruti270903 <shrutikashyap2709@gmail.com>
2025-11-27 00:37:44 +0530

    commit url's handling

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,25 +1,32 @@
 
 const fs = require('fs');
 const http= require('http');
-const path = require('path');
+const url = require("url");
 
-const server = http.createServer((req, res) => {
-    const log = `${Date.now()}:${req.url}New Request. received\n;`
- 
+const myServer = http.createServer((req, res) => {
+if(req.url === "/favicon.ico") return res.end();
+    const log = `${Date.now()}:${req.url}New Request. received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile('log.txt', log, (err, data)=>{
-        // res.end("Hello from the other side/server again ")
-        switch(req.url){
+      
+        switch(myUrl.pathname){
             case '/':
                 res.end("Hello from the homepage/server again ");
                 break;
             case '/about':
-                res.end("This is the about page of the server ");
+              const username= myUrl.query.myname;
+                res.end(`hi, ${username}`);
                 break;
                 case '/contact':
                 res.end("This is the contact page of the server ");
                 break;
-            default:
-                res.end("404 page not found ");}
+                case '/search':
+                    const search= myUrl.query.search_query;
+                    res.end("here are your result for" + search); 
+            // default:
+            //     res.end("404 page not found ");
+            }
     })
 //    console.log(req);
     console.log("New Req. received");
@@ -28,6 +35,6 @@ const server = http.createServer((req, res) => {
 });
 
 
-server.listen(8000, ()=>{
+myServer.listen(8000, ()=>{
     console.log("Server listening on port 8000 server started ");
 });
