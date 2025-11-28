4ab682fa59601340d48630d582390ed649c522f4
Shruti270903 <shrutikashyap2709@gmail.com>
2025-11-28 13:54:25 +0530

    commit rest api creation

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,24 +1,23 @@
-const http= require("http");
-const fs= require("fs");
-
-
-const myServer = http.createServer((req, res)=>{
-    // console.log("new req. found");
-    // console.log(req.headers)
-    // console.log(req)
-    const log = `${Date.now()}:${req.url} New Req Received \n`;
-    fs.appendFile("log.txt", log, (err, data)=>{
-        // res.end("Hello from server again");
-        switch(req.url){
-            case '/':res.end("homie page");
-            break;
-            case '/about': res.end("my name is shruti");
-            break;
-            default:
-                res.end("404");
-        }
-    })
-    // res.end("hello from server");
+const http = require("http");
+const fs = require("fs");
+const myServer = http.createServer((req, res) => {
+  // console.log("new req. found");
+  // console.log(req.headers)
+  // console.log(req)
+  const log = `${Date.now()}:${req.url} New Req Received \n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    // res.end("Hello from server again");
+    switch (req.url) {
+      case "/":
+        res.end("homie page");
+        break;
+      case "/about":
+        res.end("my name is shruti");
+        break;
+      default:
+        res.end("404");
+    }
+  });
+  // res.end("hello from server");
 });
-
-myServer.listen(8000, ()=> console.log("Server Started at port 1000"));
+myServer.listen(8000, () => console.log("Server Started at port 1000"));
