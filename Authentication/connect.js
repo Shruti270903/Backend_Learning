9fcee123ec44135d1642e0264aa9563f39cb2c6f
Shruti270903 <shrutikashyap2709@gmail.com>
2026-02-06 17:06:57 +0530

    new authentication folder

--- Authentication/connect.js
+++ Authentication/connect.js
@@ -1,8 +1,8 @@
-// const url = "mongodb+srv://shruti:23bca85@cluster0.6oth21r.mongodb.net/shorturl";
-// const url ="mongodb+srv://shruti:23bca85@cluster0.6oth21r.mongodb.net/shorturlwithAuth";
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 async function connectToMongoDB(url) {
   return mongoose.connect(url);
 }
-module.exports = { connectToMongoDB };
+module.exports = {
+  connectToMongoDB,
+};
--- Authentication/middleware/auth.js
+++ Authentication/middleware/auth.js
@@ -3,15 +3,12 @@ const { getUser } = require("../service/auth");
 function restrictToLoggedInUserOnly(req, res, next) {
   const userUid = req.cookies?.uid;
 
-  if (!userUid) {
-    return res.redirect("/login");
-  }
-
+  if (!userUid) return res.redirect("/login");
   const user = getUser(userUid);
 
-  if (!user) {
+  if (!user) 
     return res.redirect("/login");
-  }
+  
 
   req.user = user; 
   next();
